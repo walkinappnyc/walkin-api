d3cd65c073908a197892ef56fef47fb950593faa
Steph Reaves <stephanie.reaves@gmail.com>
2018-10-16 23:11:40 -0400

    adding separate migrations for environments

--- server/boot/migrations/custom.js
+++ server/boot/migrations/custom.js
@@ -5,29 +5,84 @@ const models = [
 ]
 
 module.exports = function migrateAccountModels (app, next) {
-  const psql = app.dataSources.testwalk
-  psql.isActual(models, (err, actual) => {
-    if (err) {
-      throw err
-    }
+  if (process.env.NODE_ENV = 'staging') {
+    console.log('staging')
+    const psql = app.dataSources.staging
+      psql.isActual(models, (err, actual) => {
+        if (err) {
+          throw err
+        }
 
-    let syncStatus = actual ? 'in sync' : 'out of sync'
+        let syncStatus = actual ? 'in sync' : 'out of sync'
 
-    console.log('')
-    console.log(`Custom models are ${syncStatus}`)
-    console.log('')
+        console.log('')
+        console.log(`Custom models are ${syncStatus}`)
+        console.log('')
 
-    if (actual) return next()
+        if (actual) return next()
 
-    console.log('Migrating Custom Models...')
+        console.log('Migrating Custom Models...')
 
-    psql.autoupdate(models, (err, result) => {
-      if (err) throw err
+        psql.autoupdate(models, (err, result) => {
+          if (err) throw err
 
-      console.log('Custom models migration successful!')
+          console.log('Custom models migration successful!')
+          console.log('')
+
+          next()
+        })
+    })
+  } else if (process.env.NODE_ENV = 'prod') {
+    console.log('prod')
+      psql.isActual(models, (err, actual) => {
+        if (err) {
+          throw err
+        }
+
+        let syncStatus = actual ? 'in sync' : 'out of sync'
+
+        console.log('')
+        console.log(`Custom models are ${syncStatus}`)
+        console.log('')
+
+        if (actual) return next()
+
+        console.log('Migrating Custom Models...')
+
+        psql.autoupdate(models, (err, result) => {
+          if (err) throw err
+
+          console.log('Custom models migration successful!')
+          console.log('')
+
+          next()
+        })
+    })
+  } else {
+    console.log('dev')
+    psql.isActual(models, (err, actual) => {
+      if (err) {
+        throw err
+      }
+
+      let syncStatus = actual ? 'in sync' : 'out of sync'
+
+      console.log('')
+      console.log(`Custom models are ${syncStatus}`)
       console.log('')
 
-      next()
+      if (actual) return next()
+
+      console.log('Migrating Custom Models...')
+
+      psql.autoupdate(models, (err, result) => {
+        if (err) throw err
+
+        console.log('Custom models migration successful!')
+        console.log('')
+
+        next()
+      })
     })
-  })
+  }
 }
