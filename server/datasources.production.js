bb26abd66b541d29345226a302bf7c41fd4d3f7e
Steph Reaves <stephanie.reaves@gmail.com>
2018-10-27 15:48:38 -0400

    adds production datasource

---
+++ server/datasources.production.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  "testwalk": {
+    "host": "",
+    "port": 5432,
+    "url": process.env.DATABASE_URL,
+    "password": "",
+    "name": "testwalk",
+    "user": "",
+    "connector": "postgresql"
+  }
+}
