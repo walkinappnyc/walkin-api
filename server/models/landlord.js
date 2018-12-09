3a38793ce2ef690a34ea3794c60a0e7e06563b2f
Stephanie Reaves <stephaniereaves@Administrators-MacBook-Pro.local>
2018-12-09 18:59:26 -0500

    adds changes for isActive and isFeatureed

--- server/models/landlord.js
+++ server/models/landlord.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(Landlord) {
+let app = require('../server')
 
+module.exports = function(Landlord) {
+  Landlord.isActive = function (data, cb) {
+    let isActive = data.isActive
+    let company = data.company
+
+    Landlord.find({where: {company: company}}, function(err, instance) {
+      console.log(`${JSON.stringify(instance)}`)
+      console.log(`${isActive}`)
+      instance[0].updateAttributes({isActive: isActive}, function(err, landlord) {
+        console.log(`${JSON.stringify(landlord)}`)
+        console.log(`${JSON.stringify(instance)}`)
+        
+        app.models.Property.find({where: {landlordId: landlord.id}}, function(err, properties) {
+          console.log(`${JSON.stringify(properties)}`)
+          properties.forEach(item => {
+            item.updateAttributes({isActive: isActive}, function(err, instance) {
+              console.log(`${instance}`)
+            })
+          })
+        })
+        return cb(null, landlord)
+      })
+    })
+  }
+
+  Landlord.remoteMethod(
+    'isActive',
+    {
+      http: {path: '/isActive', verb: 'patch'},
+      accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+      returns: { arg: 'data', type: 'object' }
+    }
+  )
 };
--- server/models/property.js
+++ server/models/property.js
@@ -1,5 +1,49 @@
 'use strict';
 
 module.exports = function(Property) {
+  Property.isActive = function (data, cb) {
+    let isActive = data.isActive
+    let xml_id = data.xml_id
 
+    Property.find({where: {xml_id: xml_id}}, function (err, instance) {
+      console.log(`${JSON.stringify(instance)}`)
+      console.log(`${isActive}`)
+      instance[0].updateAttributes({isActive: isActive}, function (err, property) {
+        console.log(`${JSON.stringify(property)}`)
+        return cb(null, property)
+      })
+    })
+  }
+
+  Property.isFeatured = function (data, cb) {
+    let isFeatured = data.isFeatured
+    let xml_id = data.xml_id
+
+    Property.find({where: {xml_id: xml_id}}, function (err, instance) {
+      console.log(`${JSON.stringify(instance)}`)
+      console.log(`${isFeatured}`)
+      instance[0].updateAttributes({isFeatured: isFeatured}, function (err, property) {
+        console.log(`${JSON.stringify(property)}`)
+        return cb(null, property)
+      })
+    })
+  }
+
+  Property.remoteMethod(
+    'isActive',
+    {
+      http: { path: '/isActive', verb: 'patch' },
+      accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+      returns: { arg: 'data', type: 'object' }
+    }
+  )
+
+  Property.remoteMethod(
+    'isFeatured',
+    {
+      http: { path: '/isFeatured', verb: 'patch' },
+      accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+      returns: { arg: 'data', type: 'object' }
+    }
+  )
 };
