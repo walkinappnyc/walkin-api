20c3750cab9b72446ba0699bd53875c7427af79a
walkinappnyc <walkinappnyc@gmail.com>
2019-01-19 16:35:10 -0500

    docker build #7

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,13 +1,13 @@
 'use strict'
 
 module.exports = {
-  'mysqlDb': {
+  'mysql': {
     'host': process.env.PRODUCTION_RDS_HOST,
     'port': 3306,
     'database': process.env.PRODUCTION_RDS_DB,
-    'user': "process.env.PRODUCTION_RDS_USER",
+    'user': process.env.PRODUCTION_RDS_USER,
     'password': process.env.PRODUCTION_RDS_PWD,
-    'name': process.env.PRODUCTION_RDS_DB,
+    'name': 'mysql',
     'connector': 'mysql',
   },
 }
--- server/datasources.staging.js
+++ server/datasources.staging.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  'mysqlDb': {
+  'mysql': {
     'host': process.env.STAGING_RDS_HOST,
     'port': 3306,
     'database': process.env.STAGING_RDS_DB,
